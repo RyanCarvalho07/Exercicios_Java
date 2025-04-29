054f8f8a6ef5e141086e1d71a92e3624dae8e6be
Ryan Carvalho <jesusryancarvalho@gmail.com>
2025-04-28 23:20:24 -0300

    Subindo mais um exercicio

---
+++ Sistema_de_Controle_Universitario/src/model/Turma.java
@@ -0,0 +1,22 @@
+package model;
+
+public class Turma {
+
+	
+	private Integer numeroDeAlunos ;
+
+	public Turma(Integer numeroDeAlunos) {
+		super();
+		this.numeroDeAlunos = numeroDeAlunos;
+	}
+
+	public Integer getNumeroDeAlunos() {
+		return numeroDeAlunos;
+	}
+
+	public void setNumeroDeAlunos(Integer numeroDeAlunos) {
+		this.numeroDeAlunos = numeroDeAlunos;
+	}
+
+	
+}
